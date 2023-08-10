ac8ed8a946fb54fdb44c2dac9b129fb304d8793c
ranaapoorv <128348354+ranaapoorv@users.noreply.github.com>
2023-08-10 15:03:02 +0530

    Add files via upload

---
+++ letsgo.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "<h4>Apoorv's Site</h4>" ;
+    echo "<hr>";//horizontal row
+
+    //data types
+    $name = "Apoorv";//char
+    $age = 20;//<int
+    $gpa = 8.13; //float
+    $select = false; //boolean
+    null ;//null
+
+    echo "$name op in the chat<br>";//using data variables
+    echo "He is $age old<br>";
+    echo "<b>Apoorv</b> ";
+    echo "is still op";
+    ?>
+</body>
+</html>
